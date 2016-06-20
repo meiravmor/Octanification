f95f631e6aab804a091bc4531c760a9ee0f40341
azulayel <eazulay@hpe.com>
2016-06-21 00:13:44 +0300

    Upload new project

---
+++ UI-Components/authentication.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace DemoApp
+{
+    partial class authentication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(authentication));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.connect = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.serverAddress = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.message = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username:";
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(434, 230);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 23);
+            this.connect.TabIndex = 2;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(353, 230);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 3;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // serverAddress
+            // 
+            this.serverAddress.Location = new System.Drawing.Point(251, 103);
+            this.serverAddress.Name = "serverAddress";
+            this.serverAddress.Size = new System.Drawing.Size(258, 20);
+            this.serverAddress.TabIndex = 4;
+            this.serverAddress.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(251, 140);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(258, 20);
+            this.username.TabIndex = 5;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(191, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(326, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Octanification Authentication \r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password:";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(251, 179);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(258, 20);
+            this.password.TabIndex = 9;
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.message.ForeColor = System.Drawing.Color.Teal;
+            this.message.Location = new System.Drawing.Point(248, 214);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(87, 13);
+            this.message.TabIndex = 10;
+            this.message.Text = "Connecting ...";
+            this.message.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(691, 266);
+            this.ControlBox = false;
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.serverAddress);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DemoApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.TextBox serverAddress;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ UI-Components/authentication.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoApp
+{
+    public partial class authentication : Form
+    {
+        int sec = 0;
+
+        public authentication()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (message.Visible)
+            {
+                timer1.Stop();
+                sec = 0;
+                faildState(true, true, true, true, true);
+                message.Visible = false;
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isValideArgs())
+            {
+                MessageBox.Show("Enter Material Name Please.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                faildState(false, false, false, false, true);
+                message.Visible = true;
+                timer1.Interval = 1000;
+                timer1.Start();
+            }
+        }
+
+        private Boolean isValideArgs()
+        {
+            Boolean valid = true;
+            if (String.IsNullOrEmpty(serverAddress.Text) || String.IsNullOrEmpty(username.Text) || String.IsNullOrEmpty(password.Text)) valid = false;
+            return valid;
+        }
+
+        private void faildState(Boolean url, Boolean user, Boolean pass, Boolean connection, Boolean close)
+        {
+            serverAddress.Enabled = url;
+            username.Enabled = user;
+            password.Enabled = pass;
+            connect.Enabled = connection;
+            cancel.Enabled = close;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            message.Text = "Connecting ... (" + sec + ")";
+
+            sec = sec + 1;
+
+            if (sec == 120)
+            {
+                timer1.Stop();
+                sec = 0;
+                MessageBox.Show("Enter Material Name Please.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                faildState(true, true, true, true, true);
+                message.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
