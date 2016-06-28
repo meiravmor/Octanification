43448a2440fe39d2307151e91fa2a612f56c8f0a
Rogov, Talia <talia.rogov@hp.com>
2016-06-28 15:11:31 +0300

    UserSetting form update

--- UI-Components/settingsWin.Designer.Designer.cs
+++ UI-Components/settingsWin.Designer.Designer.cs
@@ -30,9 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.lboxUsers = new System.Windows.Forms.ListBox();
-            this.lboxUsersNot = new System.Windows.Forms.ListBox();
-            this.lblUsers = new System.Windows.Forms.Label();
-            this.lblGetNotifications = new System.Windows.Forms.Label();
+            this.lboxUsersNotifications = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
@@ -52,43 +50,20 @@
             this.lboxUsers.Location = new System.Drawing.Point(35, 58);
             this.lboxUsers.Margin = new System.Windows.Forms.Padding(2);
             this.lboxUsers.Name = "lboxUsers";
-            this.lboxUsers.Size = new System.Drawing.Size(179, 316);
+            this.lboxUsers.Size = new System.Drawing.Size(166, 303);
             this.lboxUsers.TabIndex = 1;
-            this.lboxUsers.SelectedIndexChanged += new System.EventHandler(this.usersList_SelectedIndexChanged);
             this.lboxUsers.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lboxUsers_MouseDown);
             // 
-            // lboxUsersNot
+            // lboxUsersNotifications
             // 
-            this.lboxUsersNot.AllowDrop = true;
-            this.lboxUsersNot.FormattingEnabled = true;
-            this.lboxUsersNot.Location = new System.Drawing.Point(267, 58);
-            this.lboxUsersNot.Name = "lboxUsersNot";
-            this.lboxUsersNot.Size = new System.Drawing.Size(185, 316);
-            this.lboxUsersNot.TabIndex = 2;
-            this.lboxUsersNot.DragDrop += new System.Windows.Forms.DragEventHandler(this.lboxUsersNot_DragDrop);
-            this.lboxUsersNot.DragOver += new System.Windows.Forms.DragEventHandler(this.lboxUsersNot_DragOver);
-            // 
-            // lblUsers
-            // 
-            this.lblUsers.AutoSize = true;
-            this.lblUsers.BackColor = System.Drawing.Color.Transparent;
-            this.lblUsers.ForeColor = System.Drawing.Color.White;
-            this.lblUsers.Location = new System.Drawing.Point(35, 40);
-            this.lblUsers.Name = "lblUsers";
-            this.lblUsers.Size = new System.Drawing.Size(110, 13);
-            this.lblUsers.TabIndex = 3;
-            this.lblUsers.Text = "All uers in workspace:";
-            // 
-            // lblGetNotifications
-            // 
-            this.lblGetNotifications.AutoSize = true;
-            this.lblGetNotifications.BackColor = System.Drawing.Color.Transparent;
-            this.lblGetNotifications.ForeColor = System.Drawing.Color.White;
-            this.lblGetNotifications.Location = new System.Drawing.Point(267, 39);
-            this.lblGetNotifications.Name = "lblGetNotifications";
-            this.lblGetNotifications.Size = new System.Drawing.Size(109, 13);
-            this.lblGetNotifications.TabIndex = 4;
-            this.lblGetNotifications.Text = "Get notifications from:";
+            this.lboxUsersNotifications.AllowDrop = true;
+            this.lboxUsersNotifications.FormattingEnabled = true;
+            this.lboxUsersNotifications.Location = new System.Drawing.Point(307, 58);
+            this.lboxUsersNotifications.Name = "lboxUsersNotifications";
+            this.lboxUsersNotifications.Size = new System.Drawing.Size(169, 303);
+            this.lboxUsersNotifications.TabIndex = 2;
+            this.lboxUsersNotifications.DragDrop += new System.Windows.Forms.DragEventHandler(this.lboxUsersNotifications_DragDrop);
+            this.lboxUsersNotifications.DragOver += new System.Windows.Forms.DragEventHandler(this.lboxUsersNotifications_DragOver);
             // 
             // SettingsWin
             // 
@@ -98,9 +73,7 @@
             this.BackgroundImage = global::Octanification.Properties.Resources.hpe_logo;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(669, 431);
-            this.Controls.Add(this.lblGetNotifications);
-            this.Controls.Add(this.lblUsers);
-            this.Controls.Add(this.lboxUsersNot);
+            this.Controls.Add(this.lboxUsersNotifications);
             this.Controls.Add(this.lboxUsers);
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
@@ -114,7 +87,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "settings";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -122,8 +94,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox lboxUsers;
-        private System.Windows.Forms.ListBox lboxUsersNot;
-        private System.Windows.Forms.Label lblUsers;
-        private System.Windows.Forms.Label lblGetNotifications;
+        private System.Windows.Forms.ListBox lboxUsersNotifications;
     }
 }
